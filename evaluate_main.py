928b03a3bf78c84c6eedc116765085bc351ab905
carbuncle0228 <carbuncle0228@gmail.com>
2025-04-25 22:48:37 +0800

    only evaluate column with maco

--- evaluate_main.py
+++ evaluate_main.py
@@ -10,20 +10,17 @@ process_count = 0
 failure_count = 0
 for file_path in Path(settings.evaluate_folder_path).rglob("*"):
     if file_path.is_file():
-        if "ActionTransient.csv" not in str(file_path) and settings.DEBUG_SKIP:
-            continue
         with open(file_path, newline="", encoding="utf-8") as csvfile:
             relative_path = file_path.relative_to(
                 settings.evaluate_folder_path
             )  # Get relative path
-            reader = csv.reader(csvfile)  # Creates a CSV reader obj
+            reader = csv.reader(csvfile, delimiter=",")  # Creates a CSV reader obj
             target_file_path = settings.evaluate_target_path + str(relative_path)
             os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
             with open(
                 target_file_path, "w", buffering=1024 * 1024 * 8, encoding="utf-8"
             ) as file:
                 writer = csv.writer(file, delimiter=",")
-
                 for i, row in enumerate(reader):
                     if i < 4:
                         writer.writerow(row)
@@ -31,7 +28,10 @@ for file_path in Path(settings.evaluate_folder_path).rglob("*"):
                     try:
                         new_row: list[str] = []
                         for column in row:
-                            new_row.append(evaluated_se_string(column))
+                            if "macro(" in column:
+                                new_row.append(evaluated_se_string(column))
+                            else:
+                                new_row.append(column)
                         writer.writerow(new_row)
                     except Exception as e:
                         logging.error(
